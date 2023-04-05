2b4464de042b19ab4ed99c155a0f24e58c31e189
SadeghAT <sadegh.artivox@gmail.com>
2023-04-05 21:53:30 +0330

    background color change when click btn

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-function testLog() {
-  console.log("Clicked :)");
-}
+const btn = document.querySelector("button");
+let h2 = document.querySelector("h2");
+btn.addEventListener("click", function (e) {
+  h2.innerHTML = Number(h2.innerHTML) + 1;
+  if (Number(h2.innerHTML) >= 10) {
+    document.body.style.backgroundColor = "#291c1c";
+    console.log(h2.innerHTML);
+  } else if (Number(h2.innerHTML) >= 5) {
+    document.body.style.backgroundColor = "#1d161e";
+    console.log(h2.innerHTML);
+  } else {
+    document.body.style.backgroundColor = "#16161e";
+    console.log(h2.innerHTML);
+  }
+});
