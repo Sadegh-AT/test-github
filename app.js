201a6615203a9142d3c81b776ab7a8a2bb0c335d
sadegh121 <129953297+sadegh121@users.noreply.github.com>
2023-04-06 16:55:43 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const btn = document.querySelector("button");
 let h2 = document.querySelector("h2");
+let h1Value;
 btn.addEventListener("click", function (e) {
-  h2.innerHTML = Number(h2.innerHTML) + 1;
-  if (Number(h2.innerHTML) >= 10) {
+  h1Value = Number(h2.innerHTML) + 1;
+  h2.innerHTML = h1Value;
+  if (h1Value >= 10) {
     document.body.style.backgroundColor = "#291c1c";
     console.log(h2.innerHTML);
-  } else if (Number(h2.innerHTML) >= 5) {
+  } else if (h1Value >= 5) {
     document.body.style.backgroundColor = "#1d161e";
     console.log(h2.innerHTML);
   } else {
